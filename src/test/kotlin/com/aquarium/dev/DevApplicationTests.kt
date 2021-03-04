edf18505c873e0ab6ac3b970adf19e0852966797
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-03-04 22:09:54 +0900

    dev 21.03.04

---
+++ src/test/kotlin/com/aquarium/dev/DevApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aquarium.dev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
