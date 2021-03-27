03f41957c243904f941dda88a5dd43a82c54fdeb
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-03-27 22:40:02 +0900

    devlog 21.03.27 by nam

--- src/main/kotlin/com/aquarium/dev/config/auth/oauth/PrincipalOauth2UserService.kt
+++ src/main/kotlin/com/aquarium/dev/config/auth/oauth/PrincipalOauth2UserService.kt
@@ -2,6 +2,7 @@ package com.aquarium.dev.config.auth.oauth
 
 import com.aquarium.dev.config.auth.oauth.provider.FacebookUserInfo
 import com.aquarium.dev.config.auth.oauth.provider.GoogleUserInfo
+import com.aquarium.dev.config.auth.oauth.provider.NaverUserInfo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
@@ -11,13 +12,11 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import com.aquarium.dev.domain.repository.UserRepository
 import com.aquarium.dev.config.auth.oauth.provider.OAuth2UserInfo
+import com.aquarium.dev.domain.entity.User
+import java.util.*
 
 
-
-
-
-
-
+// 출처 : https://github.com/codingspecialist/Springboot-Security-OAuth2.0-V2/blob/master/src/main/java/com/cos/securityex01/config/oauth/PrincipalOauth2UserService.java
 // 이 부분이 실행되는 원리는? 세큐리티가 알아서 처리하는가?
 // 구글에서 받은 userRequest 데이터를 여기에서 받는다. (사후처리를 여기서 처리할 것)
 @Service
@@ -40,13 +39,20 @@ class PrincipalOauth2UserService : DefaultOAuth2UserService() {
 
         if(userRequest.clientRegistration.registrationId.equals("google")){
             println("구글 로그인 요청")
-            oAuth2UserInfo =  GoogleUserInfo(oAuth2User.attributes);
+            oAuth2UserInfo =  GoogleUserInfo(oAuth2User.attributes)
         }else if(userRequest.clientRegistration.registrationId.equals("facebook")){
             println("페이스북 로그인 요청")
-            oAuth2UserInfo =  FacebookUserInfo(oAuth2User.attributes);
+            oAuth2UserInfo =  FacebookUserInfo(oAuth2User.attributes)
+        }else if(userRequest.clientRegistration.registrationId.equals("naver")){
+            println("네이버 로그인 요청")
+            oAuth2UserInfo = NaverUserInfo(oAuth2User.attributes["response"] as Map<String, Any>)
         }else{
             println("지원하지 않은 로그인 형식입니다.")
         }
+
+//        val userOptional: Optional<User> =
+//                userRepository.findByProviderAndProviderId(oAuth2UserInfo!!.provider, oAuth2UserInfo.providerId)
+
 
 
 
