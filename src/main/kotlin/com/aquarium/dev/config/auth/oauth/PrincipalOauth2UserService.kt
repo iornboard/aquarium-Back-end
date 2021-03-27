9997fc1113c3a63003a1d993edda84a01c7718ea
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-03-28 03:14:58 +0900

    devlog 21.03.28 by nam

--- src/main/kotlin/com/aquarium/dev/config/auth/oauth/PrincipalOauth2UserService.kt
+++ src/main/kotlin/com/aquarium/dev/config/auth/oauth/PrincipalOauth2UserService.kt
@@ -4,7 +4,6 @@ import com.aquarium.dev.config.auth.oauth.provider.FacebookUserInfo
 import com.aquarium.dev.config.auth.oauth.provider.GoogleUserInfo
 import com.aquarium.dev.config.auth.oauth.provider.NaverUserInfo
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.stereotype.Service
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -13,6 +12,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import com.aquarium.dev.domain.repository.UserRepository
 import com.aquarium.dev.config.auth.oauth.provider.OAuth2UserInfo
 import com.aquarium.dev.domain.entity.User
+import com.aquarium.dev.config.auth.PrincipalDetails
+
 import java.util.*
 
 
@@ -21,10 +22,6 @@ import java.util.*
 // 구글에서 받은 userRequest 데이터를 여기에서 받는다. (사후처리를 여기서 처리할 것)
 @Service
 class PrincipalOauth2UserService : DefaultOAuth2UserService() {
-
-
-    @Autowired
-    private val bCryptPasswordEncoder : BCryptPasswordEncoder? = null
 
     @Autowired
     private val userRepository: UserRepository? = null
@@ -50,13 +47,31 @@ class PrincipalOauth2UserService : DefaultOAuth2UserService() {
             println("지원하지 않은 로그인 형식입니다.")
         }
 
-//        val userOptional: Optional<User> =
-//                userRepository.findByProviderAndProviderId(oAuth2UserInfo!!.provider, oAuth2UserInfo.providerId)
 
+        // 아래부터는 아직 이해하지못해서 급한대로 코드를 그냥 복붙해버림
+        // 추후에 공부하고 수정해야 함
+        val userOptional: Optional<User?>? =
+            userRepository!!.findByProviderAndProviderId(oAuth2UserInfo!!.provider, oAuth2UserInfo.providerId)
 
+        val user: User
+        if (userOptional!!.isPresent()) {
+            user = userOptional.get()
+            // user가 존재하면 update 해주기
+            user.userEmail = oAuth2UserInfo.email
+            userRepository.save(user)
+        } else {
+            // user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
+            user = User.builder()
+                .username(oAuth2UserInfo.provider + "_" + oAuth2UserInfo.providerId)
+                .email(oAuth2UserInfo.email)
+                .role("ROLE_USER")
+                .provider(oAuth2UserInfo.provider)
+                .providerId(oAuth2UserInfo.providerId)
+                .build()
+            userRepository.save(user)
+        }
 
-
-        return super.loadUser((userRequest))
+        return PrincipalDetails(user, oAuth2User.attributes)
     }
 
 
--- src/main/kotlin/com/aquarium/dev/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/aquarium/dev/domain/repository/UserRepository.kt
@@ -2,11 +2,12 @@ package com.aquarium.dev.domain.repository
 
 import com.aquarium.dev.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 // 출처 : https://github.com/codingspecialist/Sringboot-Security-Basic-V1/blob/master/java/com/cos/securityex01/repository/UserRepository.java
 
 interface UserRepository : JpaRepository<User?, Int?> {
     open fun findByUsername(username: String?): User? // JPA quary methed 문법을 기준으로 만들 것 (메서드 이름은 변수를 따라갈 것)
 
-
+    fun findByProviderAndProviderId(provider: String?, providerId: String?): Optional<User?>?
 }
