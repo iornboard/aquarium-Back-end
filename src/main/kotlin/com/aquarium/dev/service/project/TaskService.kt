9c87c0738b6d2973fb54f1e6fd4fc2b12a19a6dc
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-07-30 23:54:17 +0900

    dev taskServiece by nam

---
+++ src/main/kotlin/com/aquarium/dev/service/project/TaskService.kt
@@ -0,0 +1,32 @@
+package com.aquarium.dev.service.project
+
+import com.aquarium.dev.domain.dto.Project.TaskDto
+import com.aquarium.dev.domain.entity.Project.Task
+import com.aquarium.dev.domain.repository.TaskRepository
+import com.aquarium.dev.domain.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TaskService( userRepository: UserRepository, taskRepository: TaskRepository ) {
+
+//    //C
+//    fun taskCreate(taskDto: TaskDto): Task?{
+//
+//    }
+//
+//    //R
+//    fun taskRead(userId : Int) : User?{
+//
+//    }
+//
+//    //U
+//    fun taskUpdate(userDto: UserDto): User?{
+//
+//    }
+//
+//    //D
+//    fun taskDelete(userId : Int){
+//
+//    }
+
+}
